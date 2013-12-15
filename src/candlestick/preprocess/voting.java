f90f41019f9bf8272c3676f49130c6a521f8ac97
nnnone <zhiqilee@gmail.com>
2013-12-15 12:23:56 -0500

    add docs, minor revise

---
+++ src/candlestick/preprocess/voting.java
@@ -0,0 +1,11 @@
+package candlestick.preprocess;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: none
+ * Date: 12/3/13
+ * Time: 2:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class voting {
+}
